2fef268200502c250a4108fa82178d673252f36d
mshrieve <mshrieve@gmail.com>
2021-09-04 17:41:46 -0700

    update config for typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@
 // import * as dotenv from 'dotenv'
 // dotenv.config()
 
+import '@typechain/hardhat'
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-ethers'
 import 'hardhat-gas-reporter'
@@ -12,6 +13,10 @@ import './tasks'
 
 module.exports = {
   solidity: '0.8.0',
+  typechain: {
+    outDir: './types',
+    target: 'ethers-v5'
+  },
   settings: {
     optimizer: {
       enabled: true,
