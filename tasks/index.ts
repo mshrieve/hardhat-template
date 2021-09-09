72f658783d8e3e38eacb1b0a39f4dabe51661411
mike <mshrieve@gmail.com>
2021-09-09 00:29:11 -0700

    add helper tasks

--- tasks/index.ts
+++ tasks/index.ts
@@ -7,3 +7,28 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
     console.log(await account.address)
   }
 })
+
+task('getAccount', 'Prints the primary account', async (args, { ethers }) => {
+  const [owner] = await ethers.getSigners()
+
+  console.log(await owner.address)
+})
+
+task(
+  'getBalance',
+  'get balance of primary account',
+  async (args, { ethers }) => {
+    const [owner] = await ethers.getSigners()
+    const balance = await owner.getBalance()
+    console.log(ethers.utils.formatEther(balance))
+  }
+)
+
+task(
+  'randomWallet',
+  'generate random private key',
+  async (args, { ethers }) => {
+    const wallet = ethers.Wallet.createRandom()
+    console.log(wallet.privateKey)
+  }
+)
